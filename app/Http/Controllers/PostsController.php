67abdb795f6ad4bc0b57b64b41e2e1b273f34ff1
Jui-Ping <75714485+juiping97@users.noreply.github.com>
2020-12-15 09:01:55 +0800

    postcontroller.php 2

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -57,10 +57,11 @@ class PostsController extends Controller
             'image' => ['required','image'],
         ]);
 
-
         $imagePath=$request->file('image')->store('uploads','s3');
-        $image = Image::make(Storage::disk('s3')->url($imagePath))->fit(1200,1200);
+        /*
+        $image = Image::make(Storage::disk('s3')+$imagePath)->fit(1200,1200);
         $image->save();
+        */
 
         auth()->user()->posts()->create([
             'caption' => $data['caption'],
