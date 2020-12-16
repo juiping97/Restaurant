768a1125a4c3b36f0e12fa386294bf66923cdb00
Jui-Ping <75714485+juiping97@users.noreply.github.com>
2020-12-16 12:03:57 +0800

    table css

--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -1,22 +1,23 @@
 @extends('layouts.app')
 @section('content')
+
     <table class="content-table" style="font-family: 'Fredoka One', cursive">
-        <thread>
-            <tr>
-                <td>Restuarant Name</td>
-                <td>Restuarant Country</td>
-                <td>Restuarant Address</td>
-                <td>Open Time</td>
-            </tr>
-        </thread>
+        <thead>
+        <tr>
+            <th>Restuarant Name</th>
+            <th>Restuarant Country</th>
+            <th>Restuarant Address</th>
+            <th>Open Time</th>
+        </tr>
+        </thead>
         <tbody>
-            @foreach($profiles as $profile)
-                <tr>
-                    <td>{{$profile['restaurant_name']}}</td>
-                    <td>{{$profile['restaurant_country']}}</td>
-                    <td>{{$profile['restaurant_address']}}</td>
-                    <td>{{$profile['open_time']}}</td>
-                </tr>
+        @foreach($profiles as $profile)
+            <tr>
+                <td>{{$profile['restaurant_name']}}</td>
+                <td>{{$profile['restaurant_country']}}</td>
+                <td>{{$profile['restaurant_address']}}</td>
+                <td>{{$profile['open_time']}}</td>
+            </tr>
             @endforeach
         </tbody>
     </table>
