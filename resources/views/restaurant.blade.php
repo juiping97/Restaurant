58fa543a0ec7b8a7827e1105ea931c361ee85544
Jui-Ping <75714485+juiping97@users.noreply.github.com>
2020-12-16 11:54:56 +0800

    table css

--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <table class="content-table">
+    <table class="content-table" style="font-family: 'Fredoka One', cursive">
         <tr>
             <td>Restuarant Name</td>
             <td>Restuarant Country</td>
@@ -16,4 +16,48 @@
         </tr>
         @endforeach
     </table>
+
+    <style>
+        .content-table {
+            border-collapse: collapse;
+            margin: 25px 0;
+            font-size: 0.9em;
+            min-width: 400px;
+            border-radius: 5px 5px 0 0;
+            overflow: hidden;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+        }
+
+        .content-table thead tr {
+            background-color: #009879;
+            color: #ffffff;
+            text-align: left;
+            font-weight: bold;
+        }
+
+        .content-table th,
+        .content-table td {
+            padding: 12px 15px;
+        }
+
+        .content-table tbody tr {
+            border-bottom: 1px solid #dddddd;
+        }
+
+        .content-table tbody tr:nth-of-type(even) {
+            background-color: #f3f3f3;
+        }
+
+        .content-table tbody tr:last-of-type {
+            border-bottom: 2px solid #009879;
+        }
+
+        .content-table tbody tr.active-row {
+            font-weight: bold;
+            color: #009879;
+        }
+
+
+
+    </style>
 @endsection
