4dfa3af1b05b936bc365e52cd4c9bc3129b4f417
Jui-Ping <75714485+juiping97@users.noreply.github.com>
2020-12-16 11:46:08 +0800

    table css

--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 @section('content')
-    <table border="1">
+    <table class="content-table">
         <tr>
             <td>Restuarant Name</td>
             <td>Restuarant Country</td>
             <td>Restuarant Address</td>
             <td>Open Time</td>
         </tr>
+        @foreach($profiles as $profile)
         <tr>
-            @foreach($profiles as $profile)
-                <td>{{$profile['restaurant_name']}}</td>
-                <td>{{$profile['restaurant_country']}}</td>
-                <td>{{$profile['restaurant_address']}}</td>
-                <td>{{$profile['open_time']}}</td>
-            @endforeach
+            <td>{{$profile['restaurant_name']}}</td>
+            <td>{{$profile['restaurant_country']}}</td>
+            <td>{{$profile['restaurant_address']}}</td>
+            <td>{{$profile['open_time']}}</td>
         </tr>
+        @endforeach
     </table>
 @endsection
