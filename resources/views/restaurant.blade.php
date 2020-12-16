2b8a3ce044d4ef18cdca3ef2f09f1cb5b669be22
Jui-Ping <75714485+juiping97@users.noreply.github.com>
2020-12-16 12:11:20 +0800

    table css

--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -1,16 +1,39 @@
+<!doctype html>
+<html>
 @extends('layouts.app')
 @section('content')
+    <style>
+        table {
+            width:100%;
+        }
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 15px;
+            text-align: left;
+        }
+        #t01 tr:nth-child(even) {
+            background-color: #eee;
+        }
+        #t01 tr:nth-child(odd) {
+            background-color: #fff;
+        }
+        #t01 th {
+            background-color: green;
+            color: white;
+        }
+    </style>
+    <body>
 
-    <table class="content-table" style="font-family: 'Fredoka One', cursive">
-        <thead>
+    <table id="t01">
         <tr>
             <th>Restuarant Name</th>
             <th>Restuarant Country</th>
             <th>Restuarant Address</th>
             <th>Open Time</th>
         </tr>
-        </thead>
-        <tbody>
         @foreach($profiles as $profile)
             <tr>
                 <td>{{$profile['restaurant_name']}}</td>
@@ -18,48 +41,8 @@
                 <td>{{$profile['restaurant_address']}}</td>
                 <td>{{$profile['open_time']}}</td>
             </tr>
-            @endforeach
-        </tbody>
+        @endforeach
     </table>
-
-    <style>
-        .content-table {
-            border-collapse: collapse;
-            margin: 25px 0;
-            font-size: 0.9em;
-            min-width: 400px;
-            border-radius: 5px 5px 0 0;
-            overflow: hidden;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-        }
-
-        .content-table thead tr {
-            background-color: #009879;
-            color: #ffffff;
-            text-align: left;
-            font-weight: bold;
-        }
-
-        .content-table th,
-        .content-table td {
-            padding: 12px 15px;
-        }
-
-        .content-table tbody tr {
-            border-bottom: 1px solid #dddddd;
-        }
-
-        .content-table tbody tr:nth-of-type(even) {
-            background-color: #f3f3f3;
-        }
-
-        .content-table tbody tr:last-of-type {
-            border-bottom: 2px solid #009879;
-        }
-
-        .content-table tbody tr.active-row {
-            font-weight: bold;
-            color: #009879;
-        }
-    </style>
+    </body>
 @endsection
+</html>
