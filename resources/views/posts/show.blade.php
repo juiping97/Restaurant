202a1228feb3fd163b0cba8950c5b5987dfc417e
Jui-Ping <75714485+juiping97@users.noreply.github.com>
2020-12-23 12:18:24 +0800

    edit post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -37,13 +37,16 @@
                 <a href="/profile/{{$post->user->id}}" class="btn btn-primary" >Back To Profile</a>
             </div>
 
-            <a href="{{"/post/$post->id/edit"}}" class="btn btn-primary">Edit</a>
+            <div class="d-flex justify-content-between align-items-baseline pr-4">
+                <a href="{{"/post/$post->id/edit"}}" class="btn btn-primary">Edit</a>
+            </div>
 
-            <form action="{{route('post.destroy',$post->id)}}" method="post">
-                @csrf
+            <div class="d-flex justify-content-between align-items-baseline pr-4">
+                <form action="{{route('post.destroy',$post->id)}}" method="post">
+                    @csrf
                 <button class="btn btn-danger">Delete Post</button>
-            </form>
-
+                </form>
+            </div>
         </div>
     </div>
 @endsection
