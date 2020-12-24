2183ba1cbb66767db29c5f98df41b7b15f90dba4
Jui-Ping <75714485+juiping97@users.noreply.github.com>
2020-12-24 20:40:06 +0800

    push

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -32,11 +32,11 @@
             <div class="d-flex justify-content-between align-items-baseline pr-4">
                 <a href="/profile/{{$post->user->id}}" class="btn btn-primary" >Back To Profile</a>
             </div>
-            @auth
+            @if(auth()->user()->id == $post->user->id)
                 <div class="d-flex justify-content-between align-items-baseline pr-4">
                     <a href="{{"/post/$post->id/edit"}}" class="btn btn-primary">Edit</a>
                 </div>
-
+            @endif
             <div class="d-flex justify-content-between align-items-baseline pr-4">
                 <form action="{{route('post.destroy',$post->id)}}" method="post">
                     @csrf
